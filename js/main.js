30bd813ec1ee776a04a23d0572c117ed9f67b8e8
mumuyang1 <843624084@qq.com>
2014-08-16 18:10:06 +0800

    cartlist

--- js/main.js
+++ js/main.js
@@ -20,9 +20,7 @@ function getItems(){
 }
 
 
-//function
-//var categories = [];
-//
+
 //$(document).ready(function() {
 //
 //
@@ -60,49 +58,3 @@ function getItems(){
 //    );
 //   }
 //});
-//
-//$(document).ready(function(){
-//    var CartItems = getItems();
-//    localStorage.setItem('allItems', JSON.stringify(getItems()));
-//    for (var i = 0; i < items.length; i++) {
-//        if (judgeCategory(CartItems[i].category)) {
-//           categories.push(CartItems[i].category);
-//            $('#CartList').append('<div class="panel panel-warning">'+'<div class="panel-heading">'+
-//             '<div class="row text-center">'+'<div class="col-md-2">分类</div>'+
-//             '<div class="col-md-2">单价</div>'+'<div class="col-md-2">单位</div>'+
-//             '<div class="col-md-3">数量</div>'+'<div class="col-md-2">移出购物车</div>'+
-//             '<div class="col-md-1">小计</div>'+'</div>'+'</div>'+
-//             '<div class="panel-body">'+'<div class="row text-center" id="Cartitems[i].category">'+
-//             '<div class="col-md-2">'+苹果+'</div>'+'<div class="col-md-2">'+5.00元+'</div>'+
-//             '<div class="col-md-2">'+斤+'</div>'+
-//             '<div class="col-md-3 form-inline form-group">'+
-//             '<button>+</button>'+'<input type="text" value="1">'+'<button>-</button>'+
-//             '</div>'+'<div class="col-md-2">'+'<button>移除</button>'+'</div>'+
-//             '<div class="col-md-1">'+3.00元+'</div>'+'</div>'+'</div>'+'</div>');
-//        }
-//        else {
-//            $('#'+ CartItems[i].category+'').append('<div class="col-md-2">'+苹果+'</div>'+
-//            '<div class="col-md-2">'+5.00元+'</div>'+'<div class="col-md-2">'+斤+'</div>'+
-//            '<div class="col-md-3 form-inline form-group">'+
-//            '<button>+</button>'+'<input type="text" value="1">'+'<button>-</button>'+
-//            '</div>'+'<div class="col-md-2">'+'<button>移除</button>'+'</div>'+'<div class="col-md-1">'+3.00元+'</div>');
-//            }
-//         }
-//    $('#cartItemBottom').append('<p class="text-center">总计：'+1.00+'元</p>'+
-//        '<p class="text-center">'+'<a class="btn btn-primary btn-lg" role="button" href="pay.html">去结算>></a></p>');
-//});
-//
-//function judgeCategory(category){
-//    if(categories.length == 0){
-//        return true;
-//    }
-//    else {
-//        for (var j = 0; j < categories.length; j++) {
-//            if (category === categories[j]){
-//                return false;
-//            }
-//        }
-//        return true;
-//    }
-//}
-
--- js/model/pay.js
+++ js/model/pay.js
@@ -4,16 +4,21 @@
 $(document).ready(function(){
         var cartSums = JSON.parse(localStorage.getItem('cartSum'));
         $('#cartItemSum').text(cartSums);
-//        showPayList();
+        showPayList();
 });
 
-//function showPayList(){
+function showPayList(){
 
+    $('#payList1').append('<div class="text-center">打印时间：2014年8月3日</div>');
 
+    var cartProduct = JSON.parse(localStorage.getItem('cartProduct'));
+    _.forEach(cartProduct,function(cartProduct){
 
-//
-//    print='';
-//    print+=';'
-//
-//
-// }
+        $('#payList2').append('<div class="text-center">名称：'+cartProduct.items.name+
+        '，数量：'+cartProduct.inputCount+cartProduct.items.unit+'，单价：'+
+        cartProduct.items.price+'元，小计'+cartProduct.items.price*cartProduct.inputCount+'元</div>');
+    });
+
+    $('#payList3').append('<div class="text-center">总计：'+getTotal(cartProduct)+'元</div>');
+
+}
